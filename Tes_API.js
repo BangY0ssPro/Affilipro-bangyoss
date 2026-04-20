09ee02ffec0ddcf7a884b329e9b6e7ea752001be
Yusuf Siraid <yusufsiraidai@gmail.com>
2026-04-20 15:34:01 +0700

    test spreadsheet

---
+++ Tes_API.js
@@ -0,0 +1,23 @@
+import { google } from "googleapis";
+
+export default async function handler(req, res) {
+  try {
+    const auth = new google.auth.JWT(
+      process.env.GOOGLE_CLIENT_EMAIL,
+      null,
+      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    );
+
+    const sheets = google.sheets({ version: "v4", auth });
+
+    const result = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: "Sheet1!A1:B2",
+    });
+
+    res.status(200).json(result.data.values);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
